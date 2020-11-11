a6ad71ce5f15a27b51487dd1fc540a5ba1b52dc0
Tomas Kacvinsky <xkacvin1@fi.muni.cz>
2020-11-11 17:02:53 +0100

    added nicer icons

--- src/main/java/ui/Icons.java
+++ src/main/java/ui/Icons.java
@@ -7,11 +7,10 @@ import java.util.stream.Stream;
 
 final class Icons {
 
-    static final Icon DELETE_ICON = createIcon("Crystal_Clear_action_button_cancel.png");
-    static final Icon EDIT_ICON = createIcon("Crystal_Clear_action_edit.png");
-    static final Icon ADD_ICON = createIcon("Crystal_Clear_action_edit_add.png");
+    static final Icon DELETE_ICON = createIcon("delete-icon.png");
+    static final Icon EDIT_ICON = createIcon("edit-icon.png");
+    static final Icon ADD_ICON = createIcon("add-icon.png");
     static final Icon QUIT_ICON = createIcon("Crystal_Clear_action_exit.png");
-    static final Icon APPLICATION_ICON = createIcon("piggy-bank.png");
 
     private Icons() {
         throw new AssertionError("This class is not instantiable");
