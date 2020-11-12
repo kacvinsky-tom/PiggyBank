1770fb28ea532bc86592c7e2680b19bb9a5b90d3
Richard Ondrejka <485630@mail.muni.cz>
2020-11-12 11:47:13 +0100

    edited Transaction Class

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,6 +1,16 @@
 package model;
+import java.time.LocalDate;
 
 public class Transaction {
     private float amount;
-    private 
+    private String name;
+    private LocalDate date;
+    private String note;
+
+    Transaction(float amount, String name, String note){
+        this.amount = amount;
+        this.name = name;
+        this.note = note;
+    }
+    
 }
