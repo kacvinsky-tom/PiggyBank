80d405672ad9af7525559d2fe7afbe3c302150b8
Richard Ondrejka <485630@mail.muni.cz>
2020-11-12 11:51:28 +0100

    edited transactions

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -6,11 +6,14 @@ public class Transaction {
     private String name;
     private LocalDate date;
     private String note;
+    private Category category;
 
-    Transaction(float amount, String name, String note){
+    Transaction(float amount, String name, String note, Category category){
         this.amount = amount;
         this.name = name;
         this.note = note;
+        this.category = category;
+        this.date = LocalDate.now();
     }
 
 }
