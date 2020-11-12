83a83ef4af075c0c96d09e05ae6b9498753ef611
Richard Ondrejka <485630@mail.muni.cz>
2020-11-12 15:55:19 +0100

    Created statistics table

---
+++ src/main/java/ui/StatisticsTable.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javax.swing.table.AbstractTableModel;
+
+public class StatisticsTable extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+}
