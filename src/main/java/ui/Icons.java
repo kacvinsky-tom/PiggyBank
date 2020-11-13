e191bbfbe6a91bee5a7572ec42d4f2e99669232f
Filip Košár <485702@mail.muni.cz>
2020-11-13 11:24:27 +0100

    added new icons, deleted one unnecessary method

--- src/main/java/ui/Icons.java
+++ src/main/java/ui/Icons.java
@@ -1,28 +1,27 @@
 package ui;
 
 import javax.swing.*;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.awt.*;
 
 final class Icons {
 
     static final Icon DELETE_ICON = createIcon("delete-icon.png");
     static final Icon EDIT_ICON = createIcon("edit-icon.png");
     static final Icon ADD_ICON = createIcon("add-icon.png");
+    static final Image PIGGY_IMAGE = createImage("/ui/app-icon.png");
+    static final Image ADD_IMAGE = createImage("add-icon.png");
 
     private Icons() {
         throw new AssertionError("This class is not instantiable");
     }
 
-    static <E extends Enum<E>> Map<E, Icon> createEnumIcons(Class<E> clazz, int width) {
-        return Stream.of(clazz.getEnumConstants())
-                .collect(Collectors.toUnmodifiableMap(
-                        e -> e,
-                        e -> createIcon(clazz.getSimpleName() + "." + e.name() + "-" + width + ".png")));
-    }
-
     private static ImageIcon createIcon(String name) {
         return new ImageIcon(MainWindow.class.getResource(name));
     }
+
+    private static Image createImage(String name){
+        return new ImageIcon(MainWindow.class.getResource(name)).getImage();
+    }
+
+
 }
