52f2da5e381573b2511aa3fdc3b3b546626a4d95
Richard Ondrejka <485630@mail.muni.cz>
2020-11-13 15:31:25 +0100

    Created class DateAction

---
+++ src/main/java/ui/DateAction.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class DateAction extends AbstractAction {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
