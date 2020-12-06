e9d31b044557c0aeb4372488bdcb37585b3d37c7
Tomas Kacvinsky <xkacvin1@fi.muni.cz>
2020-12-06 17:48:01 +0100

    created classes TransactionDao,added attribute id to Transaction, added Transaction_Type enum

---
+++ src/main/java/data/TransactionDao.java
@@ -0,0 +1,160 @@
+package data;
+
+import model.Category;
+import model.Transaction;
+import model.TransactionType;
+
+import javax.sql.DataSource;
+import java.awt.*;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+public class TransactionDao {
+
+    private final DataSource dataSource;
+
+    public TransactionDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+        initTable();
+    }
+
+    public void create(Transaction transaction) {
+        if (transaction.getId() != null){
+            throw new IllegalArgumentException(String.format("Transaction already has ID: %s", transaction));
+        }
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement(
+                     "INSERT INTO TRANSACTION (AMOUNT, TYPE, NAME, DATE, NOTE, CATEGORY) VALUES (?, ?, ?, ?, ?, ?)",
+                     RETURN_GENERATED_KEYS)) {
+            st.setDouble(1, transaction.getAmount());
+            st.setString(2, transaction.getType().name());
+            st.setString(3, transaction.getName());
+            st.setDate(4, Date.valueOf(transaction.getDate()));
+            st.setString(5, transaction.getNote());
+            st.setLong(6, transaction.getCategory().getId());
+            st.executeUpdate();
+            try (var rs = st.getGeneratedKeys()) {
+                if (rs.next()) {
+                    transaction.setId(rs.getLong(1));
+                } else {
+                    throw new DataAccessException("Failed to fetch generated key: no key returned for transaction: " + transaction);
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to store transaction " + transaction, ex);
+        }
+    }
+
+    public void delete(Transaction transaction) {
+        if (transaction.getId() == null){
+            throw new IllegalArgumentException("Transaction has null ID");
+        }
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement(
+                     "DELETE FROM TRANSACTION WHERE ID = ?"
+             )){
+            st.setLong(1, transaction.getId());
+            int updatedRowCount = st.executeUpdate();
+            if(updatedRowCount == 0){
+                throw new DataAccessException("Failed to delete non-existing transaction: " + transaction);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to delete transaction " + transaction, ex);
+        }
+    }
+
+    public void update(Transaction transaction) {
+        if (transaction.getId() == null){
+            throw new IllegalArgumentException("Transaction has null ID");
+        }
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement(
+                     "UPDATE TRANSACTION SET AMOUNT = ? TYPE = ? NAME = ? DATE = ? NOTE = ? CATEGORY = ? WHERE ID = ?"
+             )){
+            st.setDouble(1, transaction.getAmount());
+            st.setString(2, transaction.getType().name());
+            st.setString(3, transaction.getName());
+            st.setDate(4, Date.valueOf(transaction.getDate()));
+            st.setString(5, transaction.getNote());
+            st.setLong(6, transaction.getCategory().getId());
+            st.setLong(7, transaction.getId());
+            int updatedRowCount = st.executeUpdate();
+            if(updatedRowCount == 0){
+                throw new DataAccessException("Failed to update non-existing transaction: " + transaction);
+            }
+        }  catch (SQLException ex)
+        {
+            throw new DataAccessException("Failed to update transaction " + transaction, ex);
+        }
+    }
+
+    public List<Transaction> findAll() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.prepareStatement("SELECT AMOUNT, TYPE, NAME, DATE, NOTE, CATEGORY FROM TRANSACTION")) {
+            List<Transaction> transactions = new ArrayList<>();
+            try (var rs = st.executeQuery()) {
+                while (rs.next()) {
+                    Transaction transaction = new Transaction(
+                            rs.getString("NAME"),
+                            rs.getDouble("AMOUNT"),
+                            new Category(rs.getString("CATEGORY"), Color.BLACK),
+                            rs.getDate("DATE").toLocalDate(),
+                            rs.getString("NOTE"),
+                            TransactionType.valueOf(rs.getString("TYPE")));
+                    transaction.setId(rs.getLong("ID"));
+                    transactions.add(transaction);
+                }
+            }
+            return transactions;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to load all transactions", ex);
+        }
+    }
+
+    private void initTable() {
+        if (!tableExits("APP", "TRANSACTION")) {
+            createTable();
+        }
+    }
+
+    private boolean tableExits(String schemaName, String tableName) {
+        try (var connection = dataSource.getConnection();
+             var rs = connection.getMetaData().getTables(null, schemaName, tableName, null)) {
+            return rs.next();
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to detect if the table " + schemaName + "." + tableName + " exist", ex);
+        }
+    }
+
+    private void createTable() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.createStatement()) {
+
+            st.executeUpdate("CREATE TABLE APP.TRANSACTION (" +
+                    "ID BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY," +
+                    "AMOUNT DOUBLE(30) NOT NULL," +
+                    "TYPE VARCHAR(8) NOT NULL CONSTRAINT TYPE_CHECK CHECK (TYPE IN ('INCOME','SPENDING'))," +
+                    "NAME VARCHAR(100) NOT NULL," +
+                    "DATE DATE NOT NULL" +
+                    "NOTE VARCHAR(200)" +
+                    "CATEGORY VARCHAR(100)" +
+                    ")");
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to create TRANSACTION table", ex);
+        }
+    }
+
+    public void dropTable() {
+        try (var connection = dataSource.getConnection();
+             var st = connection.createStatement()) {
+
+            st.executeUpdate("DROP TABLE APP.TRANSACTION");
+        } catch (SQLException ex) {
+            throw new DataAccessException("Failed to drop TRANSACTION table", ex);
+        }
+    }
+}
--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -3,18 +3,30 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Transaction {
+
+    private Long id;
     private double amount;
     private String name;
     private LocalDate date;
     private String note;
     private Category category;
+    private TransactionType type;
 
-    public Transaction(String name, double amount, Category category, LocalDate date, String note){
+    public Transaction(String name, double amount, Category category, LocalDate date, String note, TransactionType type){
+        setType(type);
         setName(name);
         setAmount(amount);
         setCategory(category);
         setDate(date);
         setNote(note);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public double getAmount() {
@@ -55,5 +67,13 @@ public class Transaction {
 
     public void setCategory(Category category) {
         this.category = category;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
     }
 }
