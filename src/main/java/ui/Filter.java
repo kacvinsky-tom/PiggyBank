3126ae17ed06dbd6dabb0e993ef9a86cd28a01cb
Richard Ondrejka <485630@mail.muni.cz>
2020-12-15 17:37:25 +0100

    .

--- src/main/java/ui/Filter.java
+++ src/main/java/ui/Filter.java
@@ -2,6 +2,7 @@ package ui;
 
 import model.Category;
 import model.DateSpinnerType;
+import model.Transaction;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
@@ -91,7 +92,7 @@ public class Filter {
         Calendar c = Calendar.getInstance();
         c.setTime(date);
         c.add(Calendar.DATE, i);
-        c.set(Calendar.HOUR, 0);
+        c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
         return c.getTime();
